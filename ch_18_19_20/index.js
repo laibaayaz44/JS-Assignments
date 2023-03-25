34df43da7b3b8e7b4b38a7f4e06d372caf2b5d2f
laibaayaz44 <127784346+laibaayaz44@users.noreply.github.com>
2023-03-25 23:32:30 +0500

    Add files via upload

--- ch_18_19_20/index.js
+++ ch_18_19_20/index.js
@@ -1,5 +1,25 @@
+var course=[];
+ var course1=prompt(" enter course 1" );
+ course.push(course1);
+ var course2=prompt(" enter course 2");
+ course.push(course2);
 
-let a=2;
-let b=a++ + ++a - --a + a--;
-alert("value of variable a is " + a)
-alert("value of variable b is " +b)
+ var course3=prompt(" enter course 3");
+ course.push(course3);
+
+ var course4=prompt(" enter course 4");
+ course.push(course4);
+
+ var course5=prompt(" enter course 5" );
+ course.push(course5);
+ alert(course);
+
+ // using for loop for edit and showing each course name
+
+ for(var i=0; i<course.length;i++){
+
+course[i]=prompt("edit course"+(i+1),course[i]);
+
+ }
+ //print
+ alert(course);
